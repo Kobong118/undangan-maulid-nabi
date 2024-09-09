26d1a465054feb678eee46c54641e6aeef26b025
kobong118 <kobong118@gmail.com>
2024-09-10 06:52:21 +0700

    add data

--- undangan/routes/adm.js
+++ undangan/routes/adm.js
@@ -1,23 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const app = express()
+const app = express();
+const {loadTextContent}=require('../src/loadData')
 
 router.get('/',(req,res,next)=>{
     res.redirect('/undangan-maulid-adm/muslimin')
 })
 router.get('/:kepada',(req,res,next)=>{
-    const dataProperti = {
-        teks1 : 'مولد النبي المصطفى',
-        teks2 :'Undangan Maulid Akbar',
-        logoSrc : '',
-        teks3 : 'Kepada :',
-        teks4 :' Buka Undangan',
-        teks5 :`MEMPERINGATI KELAHIRAN NABI TERAGUNG KANJENG NABI MUHANNAD SAW`,
-        teks6 : `27 Oktober 2024M - 24 Rabiul Akhir 1446H`,
-        teks7 :`مجلس التعليم<br />الأخوة دار المصطفى`,
-    }
+    const textsContents = loadTextContent(); 
+    console.log(textsContents[2].textContent)
 if(req.params.kepada !== undefined){
-    res.render('adm',{layout:'layouts/main-layout', title:`Undangan kepada ${req.params.kepada}`,nama:req.params.kepada.toUpperCase(),dataProperti,tanggal:'2024-10-27T06:00:00'})
+    res.render('adm',{layout:'layouts/main-layout', title:`Undangan kepada ${req.params.kepada}`,nama:req.params.kepada.toUpperCase(),textsContents,tanggal:'2024-10-27T06:00:00'})
 } else next()
 });
 
