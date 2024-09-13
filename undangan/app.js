0caa1a5f94bccaaaf97fe9eb1e3c3cc9fd13e9eb
kobong118 <kobong118@gmail.com>
2024-09-13 08:37:03 +0700

    add icon

--- undangan/app.js
+++ undangan/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const expressLayouts = require('express-ejs-layouts');
+const favicon = require('serve-favicon');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,6 +15,9 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+// Set favicon
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 
 app.use(logger('dev'));
 app.use(express.json());
